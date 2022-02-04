3915a541d34ef93b13ce9204b5eb73885658b88d
carlie-gorrondona <carlie.gorrondona@gmail.com>
2022-02-04 10:55:31 -0600

    part 1 and part 2 completed

--- about.js
+++ about.js
@@ -1,14 +1,20 @@
 console.log("hello world");
 
 
-
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert("Form submitted succesfully!")
+}
+
+function rubberDuckCompliment(event) {
+
+	alert("You are doing a great job!");
 }
 
 
 let form = document.querySelector('form#contact');
+let compliment = document.querySelector("#rubberduck");
 
 form.addEventListener('submit', handleSubmit);
+compliment.addEventListener('mouseover', rubberDuckCompliment);
